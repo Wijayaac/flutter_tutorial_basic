78fd50189a1f1fa7d99968ae902701a746e8e018
Wijayaac <wijayaac25@gmail.com>
2023-02-27 16:06:43 +0800

    Add : anonymous function in button click

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,38 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String message = "this is a text";
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Anonymous Method"),
+        ),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(this.message),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    message = "Button has been clicked 1 time";
+                  });
+                },
+                child: Text("Click me"))
+          ],
+        )),
+      ),
+    );
   }
 }
