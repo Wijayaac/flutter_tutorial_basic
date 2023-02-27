bbe746a04cb0b0e77db8d92509469e78139a441a
Wijayaac <wijayaac25@gmail.com>
2023-02-28 00:56:10 +0800

    Add : image assets , either using local file or external file

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Image"),
+        ),
+        body: Center(
+          child: Container(
+            width: 48,
+            height: 48,
+            padding: EdgeInsets.all(3),
+            child: Image(
+              image: AssetImage("images/shield.png"),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
