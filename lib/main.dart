5fb3c0213c563d0901a0d75746e8beccea15ec88
Wijayaac <wijayaac25@gmail.com>
2023-02-28 00:40:54 +0800

    Add : Stack and alignment widget

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,113 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Stack align widget"),
+        ),
+        body: Stack(
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Flexible(
+                    flex: 1,
+                    child: Row(
+                      children: <Widget>[
+                        Flexible(
+                            child: Container(
+                          color: Colors.white,
+                        )),
+                        Flexible(
+                            child: Container(
+                          color: Colors.black12,
+                        ))
+                      ],
+                    )),
+                Flexible(
+                    flex: 1,
+                    child: Row(
+                      children: <Widget>[
+                        Flexible(
+                            child: Container(
+                          color: Colors.black12,
+                        )),
+                        Flexible(
+                            child: Container(
+                          color: Colors.white,
+                        ))
+                      ],
+                    ))
+              ],
+            ),
+            ListView(
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Container(
+                      margin: const EdgeInsets.all(10),
+                      child: const Text(
+                        "This is a center layered text from the stack aligment widget to create overflow text",
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.all(10),
+                      child: const Text(
+                        "This is a center layered text from the stack aligment widget to create overflow text",
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.all(10),
+                      child: const Text(
+                        "This is a center layered text from the stack aligment widget to create overflow text",
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.all(10),
+                      child: const Text(
+                        "This is a center layered text from the stack aligment widget to create overflow text",
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.all(10),
+                      child: const Text(
+                        "This is a center layered text from the stack aligment widget to create overflow text",
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.all(10),
+                      child: const Text(
+                        "This is a center layered text from the stack aligment widget to create overflow text",
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.all(10),
+                      child: const Text(
+                        "This is a center layered text from the stack aligment widget to create overflow text",
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            Align(
+                alignment: Alignment(0.75, 0.90),
+                child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "My button",
+                      style: TextStyle(color: Colors.white),
+                    )))
+          ],
+        ),
+      ),
+    );
   }
 }
