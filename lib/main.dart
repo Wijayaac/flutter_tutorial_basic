6b5e103d9cb6ccfda801d4d2129f9ff71516edc3
Wijayaac <wijayaac25@gmail.com>
2023-02-28 11:39:45 +0800

    Add : post & get method

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tutorial_basic/post_result_model.dart';
+import 'package:flutter_tutorial_basic/user_model.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  PostResult postResult = null;
+  User user = null;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("HTTP API")),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Text((user != null)
+                // ignore: prefer_interpolation_to_compose_strings
+                ? user.id + " | " + user.name
+                : "Data not available"),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                ElevatedButton(
+                    onPressed: () {
+                      PostResult.connectToAPI("Wijaya", "Engineer")
+                          .then((value) {
+                        this.postResult = value;
+                        setState(() {});
+                      });
+                    },
+                    child: Text(
+                      "POST",
+                    )),
+                ElevatedButton(
+                    onPressed: () {
+                      User.connectToAPI("5").then((value) {
+                        this.user = value;
+                        setState(() {});
+                      });
+                    },
+                    child: Text(
+                      "GET User",
+                    )),
+              ],
+            )
+          ],
+        )),
+      ),
+    );
   }
 }
